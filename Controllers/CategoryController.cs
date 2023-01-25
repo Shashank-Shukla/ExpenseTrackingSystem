60fb3e4353029c2f5c0bca580748f836631473b6
S2 <shashankshowstoper@gmail.com>
2023-01-25 08:27:06 +0530

    Basic CRUD ops for Expenses

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExpenseTrackingSystem.Models.DatabaseContexts;
+using ExpenseTrackingSystem.Models.DatabaseModels;
+
+namespace ExpenseTrackingSystem.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ExpenseContext _context;
+
+        public CategoryController(ExpenseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Category
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Category/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseCategory = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (expenseCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(expenseCategory);
+        }
+
+        // GET: Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CategoryId,CategoryName,CategoryDescription,Icon,CategoryType")] ExpenseCategory expenseCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expenseCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expenseCategory);
+        }
+
+        // GET: Category/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseCategory = await _context.Categories.FindAsync(id);
+            if (expenseCategory == null)
+            {
+                return NotFound();
+            }
+            return View(expenseCategory);
+        }
+
+        // POST: Category/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CategoryId,CategoryName,CategoryDescription,Icon,CategoryType")] ExpenseCategory expenseCategory)
+        {
+            if (id != expenseCategory.CategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(expenseCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExpenseCategoryExists(expenseCategory.CategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expenseCategory);
+        }
+
+        // GET: Category/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseCategory = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (expenseCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(expenseCategory);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var expenseCategory = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(expenseCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExpenseCategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.CategoryId == id);
+        }
+    }
+}
